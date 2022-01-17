9a7a4d439af621ce893d33da4a825368c12e982e
umesh <rajputumesh117@gmail.com>
2022-01-17 12:30:46 +0530

    js project

---
+++ data.js
@@ -0,0 +1,226 @@
+const arraydata = [
+    [
+      {
+        "key": "sector",
+        "value": "Basic Materials"
+      },
+      {
+        "key": "perChange",
+        "value": "-0.52"
+      },
+      {
+        "key": "noOfStocks",
+        "value": "0"
+      }
+    ],
+    [
+      {
+        "key": "sector",
+        "value": "Communications"
+      },
+      {
+        "key": "perChange",
+        "value": "6.00"
+      },
+      {
+        "key": "noOfStocks",
+        "value": "0"
+      }
+    ],
+    [
+      {
+        "key": "sector",
+        "value": "Communication Services"
+      },
+      {
+        "key": "perChange",
+        "value": "-3.29"
+      },
+      {
+        "key": "noOfStocks",
+        "value": "0"
+      }
+    ],
+    [
+      {
+        "key": "sector",
+        "value": "Conglomerates"
+      },
+      {
+        "key": "perChange",
+        "value": "-4.00"
+      },
+      {
+        "key": "noOfStocks",
+        "value": "0"
+      }
+    ],
+    [
+      {
+        "key": "sector",
+        "value": "Consumer Cyclical"
+      },
+      {
+        "key": "perChange",
+        "value": "-4.39"
+      },
+      {
+        "key": "noOfStocks",
+        "value": "0"
+      }
+    ],
+    [
+      {
+        "key": "sector",
+        "value": "Consumer Defensive"
+      },
+      {
+        "key": "perChange",
+        "value": "-2.84"
+      },
+      {
+        "key": "noOfStocks",
+        "value": "0"
+      }
+    ],
+    [
+      {
+        "key": "sector",
+        "value": "Energy"
+      },
+      {
+        "key": "perChange",
+        "value": "3.74"
+      },
+      {
+        "key": "noOfStocks",
+        "value": "0"
+      }
+    ],
+    [
+      {
+        "key": "sector",
+        "value": "Financial Services"
+      },
+      {
+        "key": "perChange",
+        "value": "4.14"
+      },
+      {
+        "key": "noOfStocks",
+        "value": "0"
+      }
+    ],
+    [
+      {
+        "key": "sector",
+        "value": "Healthcare"
+      },
+      {
+        "key": "perChange",
+        "value": "-6.44"
+      },
+      {
+        "key": "noOfStocks",
+        "value": "0"
+      }
+    ],
+    [
+      {
+        "key": "sector",
+        "value": "Industrials"
+      },
+      {
+        "key": "perChange",
+        "value": "-2.63"
+      },
+      {
+        "key": "noOfStocks",
+        "value": "0"
+      }
+    ],
+    [
+      {
+        "key": "sector",
+        "value": "Miscellaneous"
+      },
+      {
+        "key": "perChange",
+        "value": "-14.29"
+      },
+      {
+        "key": "noOfStocks",
+        "value": "0"
+      }
+    ],
+    [
+      {
+        "key": "sector",
+        "value": "Others"
+      },
+      {
+        "key": "perChange",
+        "value": "-1.19"
+      },
+      {
+        "key": "noOfStocks",
+        "value": "0"
+      }
+    ],
+    [
+      {
+        "key": "sector",
+        "value": "Real Estate"
+      },
+      {
+        "key": "perChange",
+        "value": "-3.78"
+      },
+      {
+        "key": "noOfStocks",
+        "value": "0"
+      }
+    ],
+    [
+      {
+        "key": "sector",
+        "value": "Services"
+      },
+      {
+        "key": "perChange",
+        "value": "-19.12"
+      },
+      {
+        "key": "noOfStocks",
+        "value": "0"
+      }
+    ],
+    [
+      {
+        "key": "sector",
+        "value": "Technology"
+      },
+      {
+        "key": "perChange",
+        "value": "-5.04"
+      },
+      {
+        "key": "noOfStocks",
+        "value": "0"
+      }
+    ],
+    [
+      {
+        "key": "sector",
+        "value": "Utilities"
+      },
+      {
+        "key": "perChange",
+        "value": "-2.52"
+      },
+      {
+        "key": "noOfStocks",
+        "value": "0"
+      }
+    ]
+  ]
